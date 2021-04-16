3f83e9426d9f7a3d628a74d316dab8a438c0c420
JeZoS <prabhat262111@gmail.com>
2021-04-16 15:45:09 +0530

    created Models

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser  
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+class Lead(models.Model):
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent",on_delete=models.CASCADE)
+
+class Agent(models.Model):
+
+    def __str__(self):
+        return self.user.username 
+    
+
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lead
 
 # Create your views here.
+def lead_list(requests):
+    # return HttpResponse("Hello World")
+    leads = Lead.objects.all();
+    context = {
+        "leads":leads
+    }
+    return render(requests,'leads/home_page.html',context)
+
+def lead_detail(requests,pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead":lead
+    }
+    return render(requests,'leads/lead_detail.html',context)
